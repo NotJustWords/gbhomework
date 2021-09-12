057f4806889392f0fd2094a1ef9be4ff6dd66e41
NotJustWords <upofme1@gmail.com>
2021-09-12 19:39:47 +0300

    lesson-5 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.IO;
 
 namespace Task1
 {
+    /*
+     * Ввести с клавиатуры произвольный набор данных и сохранить 
+     * его в текстовый файл.
+     */
     class Program
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            File.AppendAllLines(input, new[] { input });
         }
     }
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            
+            File.AppendAllLines("startup.txt", new[] { DateTime.Now.ToString()});
+            Console.WriteLine(File.ReadAllText("startup.txt"));
+        }
+    }
+}
