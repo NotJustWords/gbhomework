2d84271fdf8fe186a3a4c662a75b192093aa0bf3
NotJustWords <upofme1@gmail.com>
2021-09-15 18:11:54 +0300

    lesson-6 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Task1
 {
@@ -6,7 +7,38 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            bool isTrue = true;
+
+            while (isTrue)
+            {
+
+                foreach (Process process in Process.GetProcesses())
+                {
+                    Console.WriteLine($"|Id: { process.Id,-5}| Name: {process.ProcessName,-18}\n|");                    
+                }
+
+                Console.Write("Введите q, чтобы выйти.\n\nВведите Id процесса, который вы хотите" +
+                    " завершить: ");
+                string UsersId = Console.ReadLine();
+                if (UsersId == "q") isTrue = false;
+                else SetProcess(int.Parse(UsersId));               
+
+            }
+
+        }
+
+        static void SetProcess(int usersId)
+        {
+            Process ProcessById = Process.GetProcessById(usersId);
             
+            try
+            {
+                ProcessById.Kill();
+            }
+            catch (ArgumentException)
+            {
+                throw new Exception("Процесса с таким Id не существует.");
+            }
         }
     }
 }
